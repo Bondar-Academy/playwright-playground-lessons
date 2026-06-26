5211e90a64a73929be2056dc39eee2ef06995ddf
Artem Bondar <artem.bondar@gmail.com>
2026-06-26 12:37:33 -0500

    user-visible locators

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -35,8 +35,17 @@ test('Locator Syntax Rules', async ({ page }) => {
     page.locator(':text-is("Using the Grid")')
 })
 
+test('User-visible locators', async ({page}) => {
+    await page.getByRole('button', {name: 'Sign in'}).first().click()
+    await page.getByRole('textbox', {name: "Email"}).first().fill('test@test.com')
 
+    await page.getByLabel('Email').first().fill('test@test.com')
 
+    await page.getByPlaceholder('Jane Doe').fill('Artem Bondar')
 
+    await page.getByText('Submit').first().click()
 
+    await page.getByTestId('inputEmail1').fill('test@test.com')
 
+    await page.getByTitle('IoT Dashboard').click()
+})
