30b00a4d54ea423c06165fc2a6d3e1401e396d52
Artem Bondar <artem.bondar@gmail.com>
2026-07-03 15:40:01 -0500

    checkboxes

--- tests/ui-components.spec.ts
+++ tests/ui-components.spec.ts
@@ -39,3 +39,16 @@ test.describe('Form Layouts page', () => {
         await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).not.toBeChecked()
     })
 })
+
+test('checkboxes', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.check({force: true})
+        await expect(box).toBeChecked()
+    }
+})
