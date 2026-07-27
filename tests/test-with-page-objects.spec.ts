7fc8909161ed74aafb6bb47b898ef96746c6a498
Artem Bondar <artem.bondar@gmail.com>
2026-07-27 14:43:07 -0500

    datepicker page object

--- tests/test-with-page-objects.spec.ts
+++ tests/test-with-page-objects.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigation-page'
 import { FormLayoutsPage } from '../page-objects/form-layouts-page'
+import { DatepickerPage } from '../page-objects/datepicker-page'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://playground.bondaracademy.com/')
@@ -17,7 +18,11 @@ test('Navigate to form layouts page', async ({ page }) => {
 test('Parametrized page object methods', async({page}) => {
     const navigateTo = new NavigationPage(page)
     const formLayoutsPage = new FormLayoutsPage(page)
+    const datepickerPage = new DatepickerPage(page)
     await navigateTo.formLayoutsPage()
     await formLayoutsPage.submitUsingTheGridForm('artem@test.com', 'Welcome', 'Option 2')
     await formLayoutsPage.submitInlineForm('Artem Bondar', 'artem@test.com', false)
+    await navigateTo.datePickerPage()
+    await datepickerPage.selectCommonDatepickerDateFromToday(5)
+    await datepickerPage.selectDatePickerWithDangeFromToday(7, 20)
 })
