6958d1472b8a199d03c3240d6366a1edfcd2eb91
Artem Bondar <artem.bondar@gmail.com>
2026-07-28 12:27:56 -0500

    page object manager

--- tests/test-with-page-objects.spec.ts
+++ tests/test-with-page-objects.spec.ts
@@ -1,28 +1,25 @@
 import { test } from '@playwright/test'
-import { NavigationPage } from '../page-objects/navigation-page'
-import { FormLayoutsPage } from '../page-objects/form-layouts-page'
-import { DatepickerPage } from '../page-objects/datepicker-page'
+import { PageManager } from '../page-objects/page-manager'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://playground.bondaracademy.com/')
 })
 
 test('Navigate to form layouts page', async ({ page }) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datePickerPage()
-    await navigateTo.toasterPage()
-    await navigateTo.smartTablePage()
+    const pom = new PageManager(page)
+    await pom.navigateTo.formLayoutsPage()
+    await pom.navigateTo.datePickerPage()
+    await pom.navigateTo.toasterPage()
+    await pom.navigateTo.smartTablePage()
 })
 
 test('Parametrized page object methods', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const formLayoutsPage = new FormLayoutsPage(page)
-    const datepickerPage = new DatepickerPage(page)
-    await navigateTo.formLayoutsPage()
-    await formLayoutsPage.submitUsingTheGridForm('artem@test.com', 'Welcome', 'Option 2')
-    await formLayoutsPage.submitInlineForm('Artem Bondar', 'artem@test.com', false)
-    await navigateTo.datePickerPage()
-    await datepickerPage.selectCommonDatepickerDateFromToday(5)
-    await datepickerPage.selectDatePickerWithRangeFromToday(7, 20)
+    const pom = new PageManager(page)
+    await pom.navigateTo.formLayoutsPage()
+    await pom.formLayoutsPage.submitUsingTheGridForm('artem@test.com', 'Welcome', 'Option 2')
+    await pom.formLayoutsPage.submitInlineForm('Artem Bondar', 'artem@test.com', false)
+    await pom.navigateTo.datePickerPage()
+    await pom.datepickerPage.selectCommonDatepickerDateFromToday(5)
+    await pom.datepickerPage.selectDatePickerWithRangeFromToday(7, 20)
+    await pom.navigateTo.smartTablePage()
 })
